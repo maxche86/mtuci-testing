d9bb409f4167065c538a6c18b46c91fd8b9a91f9
Maxim Chernoshekov <maxim.chernoshekov@ic-group.ru>
2025-08-11 10:21:50 +0300

    fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/mtuci-testing/',
+  base: '/',
   build: {
     outDir: 'dist',
   },
