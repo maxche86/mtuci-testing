7f848495775c30cee10cefb580a07bd313e19357
Maxim Chernoshekov <maxim.chernoshekov@ic-group.ru>
2025-08-13 11:45:06 +0300

    refactored store

--- src/widgets/main-introduction/ui/main-intruduction.tsx
+++ src/widgets/main-introduction/ui/main-intruduction.tsx
@@ -6,12 +6,12 @@ import * as styles from './main-introduction.css.ts';
 
 export const MainIntroduction: FC = () => {
   const { isMobile } = useMatchMedia();
-  const totalQntQuestion = useTestStore.use.currentTest().tests.length;
+  const totalQntQuestion = useTestStore.use.currentTest();
 
   return (
     <Flex className={isMobile ? styles.container.mobile : styles.container.desktop}>
       <Text className={isMobile ? styles.text.mobile : styles.text.desktop} span>
-        Тест состоит из {totalQntQuestion} вопросов <br />
+        Тест состоит из {totalQntQuestion.tests.length} вопросов <br />
         Общее время данное на прохождение: 80 минут <br />
         Тест запустится сразу после нажатия на кнопку “Начать”
       </Text>
